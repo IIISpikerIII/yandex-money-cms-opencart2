e78b2b8ee9dd65aa33523bf9734189f801727792
Nikita Zorin <nikita.zorin@convergent-usa.com>
2016-11-13 10:39:24 +0300

    проверка статуса рассчетов доставки

--- src/upload/catalog/controller/yandexbuy/cart.php
+++ src/upload/catalog/controller/yandexbuy/cart.php
@@ -199,7 +199,7 @@ class ControllerYandexbuyCart extends Controller
 							$id = $result['code'];
 							$types = $this->config->get('ya_pokupki_carrier');
 							$type = isset($types[$id]) ? $types[$id] : 'POST';
-							if ($quote && $quote['quote']['error'] === false)
+							if ($quote && $quote['error'] === false)
 							{
 								$carriers[$k] = array(
 									'id' => $result['extension_id'],
